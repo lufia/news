6f8c3d5b0b27b09a27ef18bf80551e69e1cbcbc2
lufia <lufia@me.com>
2015-09-09 00:06:55 +0900

    rss: add rss 1.0 support

---
+++ rss/rss.go
@@ -0,0 +1,45 @@
+package rss
+
+import (
+	"encoding/xml"
+	"io"
+	"time"
+)
+
+type Feed struct {
+	XMLName xml.Name `xml:"RDF"`
+	Channel *Channel `xml:"channel"`
+	Items   []*Item  `xml:"item"`
+}
+
+type Channel struct {
+	Title       string    `xml:"title"`
+	Link        string    `xml:"link"`
+	Description string    `xml:"description"`
+	Date        time.Time `xml:"date"`
+	Language    string    `xml:"language"`
+	Indexes     []*Index  `xml:"items>Seq>li"`
+}
+
+type Index struct {
+	URL string `xml:"resource,attr"`
+}
+
+type Item struct {
+	Title       string    `xml:"title"`
+	Link        string    `xml:"link"`
+	Description string    `xml:"description"`
+	Creator     string    `xml:"creator"`
+	Date        time.Time `xml:"date"`
+}
+
+func Parse(r io.Reader) (feed *Feed, err error) {
+	var x Feed
+	d := xml.NewDecoder(r)
+	err = d.Decode(&x)
+	if err != nil {
+		return
+	}
+	feed = &x
+	return
+}
---
+++ rss/rss_test.go
@@ -0,0 +1,77 @@
+package rss
+
+import (
+	"encoding/xml"
+	"strings"
+	"testing"
+)
+
+func TestParse(t *testing.T) {
+	tab := []struct {
+		XMLString string
+		Expected  *Feed
+	}{
+		{
+			XMLString: xmlStringSimple,
+			Expected: &Feed{
+				XMLName: xml.Name{
+					Space: "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+					Local: "RDF",
+				},
+			},
+		},
+	}
+	for _, v := range tab {
+		r := strings.NewReader(v.XMLString)
+		feed, err := Parse(r)
+		if err != nil {
+			t.Errorf("Parse(%q) = %v", v.XMLString, err)
+			continue
+		}
+		t.Logf("%v\n", *feed)
+		t.Logf("%v\n", feed.Channel)
+		for _, p := range feed.Items {
+			t.Logf("\t%v\n", *p)
+		}
+	}
+}
+
+var xmlStringSimple = strings.TrimSpace(`
+<?xml version="1.0" encoding="UTF-8"?>
+<rdf:RDF xmlns="http://purl.org/rss/1.0/"
+	xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+	xmlns:dc="http://purl.org/dc/elements/1.1/"
+	xmlns:content="http://purl.org/rss/1.0/modules/content/"
+	xml:lang="ja">
+
+	<channel rdf:about="サイトのRSSのURL">
+		<title>サイトのタイトル</title>
+		<link>サイトのURL</link>
+		<description>サイトの内容</description>
+		<dc:date>2015-02-01T02:03:45Z</dc:date>
+		<dc:language>ja</dc:language> 
+		<items>
+		<rdf:Seq>
+		<rdf:li rdf:resource="記事1のURL" />
+		<rdf:li rdf:resource="記事2のURL" />
+		</rdf:Seq>
+		</items>
+	</channel>
+
+	<item rdf:about="記事1のURL">
+		<title>記事1のタイトル</title>
+		<link>記事1のURL</link>
+		<description><![CDATA[記事1の内容]]></description>
+		<dc:creator>記事1の作者名</dc:creator>
+		<dc:date>2015-01-01T02:03:45+09:00</dc:date>
+	</item>
+
+	<item rdf:about="記事2のURL">
+		<title>記事2のタイトル</title>
+		<link>記事2のURL</link>
+		<description><![CDATA[記事2の内容]]></description>
+		<dc:creator>記事2の作者名</dc:creator>
+		<dc:date>2015-02-01T02:03:45+09:00</dc:date>
+	</item>
+</rdf:RDF>
+`)
